ebc5144b6d82a2dfdd29a365043f0c48f58f005b
Carl Marino <carl@ccm.me>
2023-08-13 15:07:53 +0200

    started stuff

---
+++ scroll.js
@@ -0,0 +1,31 @@
+var scroll = new LocomotiveScroll({
+    el: document.querySelector('[data-scroll-container]'),
+    smooth: true
+});
+
+scroll.on('scroll', (obj) => {
+    pageHandler(obj.delta.y)
+});
+
+function pageHandler(deltaY) {
+    let pageNum = Math.floor(deltaY/window.innerHeight)
+    console.log(pageNum)
+    switch (pageNum) {
+        case 0:
+            document.documentElement.style.setProperty("--background-color", "#EEE5D8");
+            break
+        case 1:
+            document.documentElement.style.setProperty("--background-color", "#C6C6C6");
+            break;
+        case 2:
+            document.documentElement.style.setProperty("--background-color", "#FFFFFF");            
+            break;
+        case 3:
+            document.documentElement.style.setProperty("--background-color", "#C4C9D3");
+            break;
+        case 4:
+            document.documentElement.style.setProperty("--background-color", "#FFFFFF");
+            break;
+
+    }
+}
