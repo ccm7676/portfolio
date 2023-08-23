cfdd4ee7d8427cd9a664a435c31bc1c44ef68c6d
Carl Marino <carl@ccm.me>
2023-08-23 21:05:03 +0200

    made animations and popup functionality for menu

--- btn-handler.js
+++ btn-handler.js
@@ -1,7 +1,34 @@
 const readMoreBtn = document.querySelector(".read-more");
 const aboutPage = document.querySelector(".about-home");
+const menuBtn = document.querySelector("svg.hamburger");
+const hideDots = document.querySelectorAll(".xhide");
 
 
+
+menuBtn.addEventListener("click", () => {
+    let docStyle = document.documentElement.style;
+
+    if(docStyle.getPropertyValue("--menu-state") == "showmenu"){
+
+        docStyle.setProperty("--menu-state", "hidemenu");
+
+        hideDots.forEach((el) => {
+            el.style.opacity = 1;
+        }); 
+
+    }
+
+    else {
+
+        docStyle.setProperty("--menu-state", "showmenu")
+
+        hideDots.forEach((el) => {
+            el.style.opacity = 0;
+        }); 
+
+    }
+        
+})
 
 readMoreBtn.addEventListener("click", () => {
     if(readMoreBtn.innerHTML == "READ MORE"){
