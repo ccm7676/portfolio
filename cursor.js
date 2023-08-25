f6f5831de690e38cb0fe3afc0947f24bfac1dab6
Carl Marino <carl@ccm.me>
2023-08-25 19:17:56 +0200

    finished custom cursor

---
+++ cursor.js
@@ -0,0 +1,35 @@
+const cursor = document.querySelector(".cursor-container");
+const mainBtns = document.querySelectorAll(".btn");
+let Btns = Array.from(mainBtns);
+Btns.push(document.querySelector(".ham-container"))
+
+window.addEventListener("mousemove", (e) => {
+    let vh = window.innerHeight/100;
+    cursor.style.top = e.clientY - 2.5*vh + "px";
+    cursor.style.left = e.clientX - 2.5*vh + "px";
+})
+
+window.addEventListener("mousedown", ()=> {
+    cursor.classList.add("click")
+})
+
+window.addEventListener("mouseup", ()=> {
+    cursor.classList.remove("click")
+})
+
+document.documentElement.addEventListener("mouseenter", () => {
+    document.documentElement.style.setProperty("--cursor-state","showcursor")
+})
+
+document.documentElement.addEventListener("mouseleave", () => {
+    document.documentElement.style.setProperty("--cursor-state","hidecursor")
+})
+
+Btns.forEach((el) => {
+    el.addEventListener("mouseenter", () => {
+        document.documentElement.style.setProperty("--cursor-state","hidecursor")
+    })
+    el.addEventListener("mouseleave", () => {
+        document.documentElement.style.setProperty("--cursor-state","showcursor")
+    })
+})
