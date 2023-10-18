3ae14c7af31056f711d271f4150799a51b1feccf
Carl Marino <carl@ccm.me>
2023-10-18 18:07:21 +0200

    started music page

---
+++ player.js
@@ -0,0 +1,8 @@
+
+
+songs = ["azure.mp3","moon.mp3", "coffee.mp3"]
+years = ["2023", "2021", "2022"]
+songNames = ["Azure Odyssey", "The Moon", "Cozy Coffee"]
+covers = ["azure-cover.avif","moon-cover.avif", "coffee-cover.avif"]
+
+selected = 0
